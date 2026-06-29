f39e31daf0c98ed4c756d2bc90a73bbe89b58ffe
mishthimahajan <mishthimahajan0@gmail.com>
2026-06-29 14:05:55 +0530

    Fix CORS

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://enterprise-rag-2ux4.onrender.com/api",
 });
 
 export default api;
